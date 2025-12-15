93f0b5f8cb40097e759ba6ac9fdbb1be24e5caa8
Anit <anit7795@gmail.com>
2025-12-15 21:41:42 +0530

    connect mongodb and gitignore

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+
+export const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Connected Successfully!')
+    } catch (e) {
+        console.log('Cannot connect to the database!', e);
+        process.exit(1);
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import router from "./routes/notesRoutes.js";
+import { connectDb } from "../config/db.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+connectDb();
 
 app.use('/notes/api', router)
 
