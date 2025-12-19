3b56fe0386e75ab8d9fb92705c5ededabf811823
Anit <anit7795@gmail.com>
2025-12-20 01:05:18 +0530

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,13 @@ import NoteDetailPage from './pages/NoteDetailPage';
 
 const App = () => {
   return (
-        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-size-[16px_16px]">
            <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/create' element={<CreatePage />} />
           <Route path='/note/:id' element={<NoteDetailPage />} />
         </Routes> 
             </div>
-        
-     
   );
 };
 
