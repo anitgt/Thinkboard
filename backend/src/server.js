2cbdbd3860a451b254724929205134e763c6050e
Anit <anit7795@gmail.com>
2025-12-20 01:37:58 +0530

    Ready for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
-import cors from  'cors'
+import cors from  'cors';
+import path from  'path';
 
 import router from "./routes/notesRoutes.js";
 import { connectDb } from "../config/db.js";
@@ -10,17 +11,26 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve();
 
-
-
-app.use(express.json())
-
-app.use(cors({
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
     origin: "http://localhost:5173"
 }))
+}
+
+app.use(express.json())
 app.use(rateLimiter);
 
-app.use('/api/notes', router)
+app.use('/api/notes', router);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+})
+}
 
 connectDb().then(() => {
    app.listen(PORT, () => {
